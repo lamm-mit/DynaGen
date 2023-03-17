8349da89c299871904ff3734f34296f91c8d9d81
LAMM: MIT Laboratory for Atomistic and Molecular Mechanics <101393859+lamm-mit@users.noreply.github.com>
2023-03-17 14:55:22 -0400

    Add files via upload

--- DynaGen/__init__.py
+++ DynaGen/__init__.py
@@ -1,16 +1,11 @@
 from DynaGen.DynaGen import DynaGen, Unet
 from DynaGen.DynaGen import NullUnet
-#from DynaGen.DynaGen import BaseUnet64, SRUnet256, SRUnet1024
+
 from DynaGen.trainer import DynaGenTrainer
 from DynaGen.version import __version__
 
-# imagen using the elucidated ddpm from Tero Karras' new paper
-
+# DynaGen using the elucidated ddpm from Tero Karras et al.
 from DynaGen.elucidated_DynaGen import ElucidatedDynaGen
-
-# config driven creation of imagen instances
-
-#from DynaGen.configs import UnetConfig, ImagenConfig, ElucidatedImagenConfig, ImagenTrainerConfig
 
 # utils
 
